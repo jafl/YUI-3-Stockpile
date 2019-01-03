744d68b9e5849fb01a825a835543a8094b5b3518
John Lindal <jafl@users.noreply.github.com>
2019-01-03 15:16:28 -0800

    Remove unused variable

--- server/hbs-helpers.js
+++ server/hbs-helpers.js
@@ -3,7 +3,6 @@ var mod_hbs = require('express-hbs');
 mod_hbs.registerHelper('each-with-last', function(list, options)
 {
 	var total  = list.length,
-		last   = total-1,
 		buffer = '';
 
 	for (var i=0; i<total; i++)
